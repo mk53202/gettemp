243f51605d12a627331e2c337564380a38f60f7c
Taco Boy <taco@Tacos-MacBook-Pro.local>
2018-05-28 19:55:28 -0500

    renamed to server.py

--- sensor_temperature.py
+++ sensor_temperature.py
@@ -2,6 +2,10 @@ import time
 import datetime
 import random
 
+# Define a function to convert celsius to fahrenheit.
+def c_to_f(c):
+	return c * 9.0 / 5.0 + 32.0
+
 def get_datetime():
     return datetime.datetime.now()
 
